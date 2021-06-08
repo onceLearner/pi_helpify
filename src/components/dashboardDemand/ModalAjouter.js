d494c388f3dd25553a66a22fcc6378e2877d1bcf
OnceLearner <redaamir972@gmail.com>
2021-06-08 16:57:04 +0100

    kjsdflj :

--- src/components/dashboardDemand/ModalAjouter.js
+++ src/components/dashboardDemand/ModalAjouter.js
@@ -97,7 +97,7 @@ const ModalAjouter = ({ openModalHook, setListDemandes, listDemandes, userInfo, 
     return (
         <div >
             <div className="fixed  z-20 bg-black opacity-70 top-0 right-0 left-0 bottom-0 " style={{ fontFamily: "Montserrat" }}></div>
-            <div className={`fided z-30 top-12 md:right-20 md:left-20 left-10 right-10 bg-white flex flex-col p-3  transform scale-${ModalScale}  transition-transform ease-in rounded-xl`} style={{ transitionDuration: "200ms" }}>
+            <div className={`fixed z-30  top-12 md:right-20 md:left-20 left-10 right-10 bg-white flex flex-col p-3  transform scale-${ModalScale}  transition-transform ease-in rounded-xl`} style={{ transitionDuration: "200ms" }}>
                 <button className="absolute right-3 top-2 hover:text-red-400  " onClick={() => setOpenModal(false)}><IconCloseModal /></button>
 
                 <div className=" flex flex-col   " style={{ minHeight: "500px" }}>
